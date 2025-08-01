3a3c49ea149da5e311f29333e5dd5903fe23faca
Mohammed Qasim <mohammed.qasim1702@gmail.com>
2025-08-01 22:35:07 +0530

    init

---
+++ script.js
@@ -0,0 +1,108 @@
+// console.log("Hello World!");
+
+// var name = "John";
+// console.log(name);
+
+// var colleageTeam = "Footbaall";
+// var collegeNumber = 8;
+// console.log(collegeNumber);
+
+// var isRegistered = true;
+// console.log(isRegistered);
+
+// var work;
+// console.log(work);
+
+// work = "Full Stack Developer";
+// console.log(work);
+
+// console.log(name + " " + collegeNumber);
+
+// var colleageName, hasCourse;
+
+// colleageName = "MHSSCE";
+// hasCourse = true;
+
+// console.log(
+//   colleageName + " has 35 courses. Does it have a course? " + hasCourse
+// );
+
+// collegeNumber = "Eleven";
+// console.log(collegeNumber);
+
+// colleageTeam = "Soccer";
+// console.log(colleageTeam);
+
+// alert(colleageTeam);
+
+// var age = prompt("What is your age?");
+// console.log("Your age is " + age + " years old.");
+
+//* Basic Operators
+// var totalDistance, distanceJohn, distanceMessi;
+// totalDistance = 20;
+
+// distanceJohn = totalDistance - 12;
+// console.log(distanceJohn);
+// distanceMessi = totalDistance - 7;
+// console.log(distanceMessi);
+// console.log(distanceMessi * 2);
+// console.log(distanceMessi / 2);
+
+// // logical operators
+// var distanceToCollege, distanceToHome;
+
+// distanceToCollege = 8;
+// distanceToHome = 12;
+
+// var isClose = distanceToHome > distanceToCollege;
+// console.log(isClose);
+
+// // Boolean
+// var isLoggedIn = true;
+
+// // Logical Operators
+// var a, b;
+// (a = 15), (b = 11);
+// console.log(a > b || !(b != "11" && a == "15"));
+
+//* Conditional Statements
+// var grade;
+// grade = 42;
+
+// if (grade < 40) {
+//   console.log("You can't go to next semester.");
+// } else if (grade >= 40 && grade < 45) {
+//   console.log("You can go to next semester, but you can't enter the exam.");
+// } else {
+//   console.log("You can go to next semester.");
+// }
+
+// var age;
+// age = 19;
+
+// if (age < 18) {
+//   console.log("You can't enter the cafe.");
+// } else if (age < 23) {
+//   console.log("You can enter the cafe, but you can't order alcohol.");
+// } else {
+//   console.log("You can enter the cafe and order alcohol.");
+// }
+
+// // Ternary Operator
+// var grade = 80;
+
+// grade < 45
+//   ? console.log("You can't can't go to next semester.")
+//   : console.log("You can go to next semester.");
+
+// var result = grade < 45 ? "Failure" : "Success";
+// console.log(result);
+
+// if (grade < 45) {
+//   console.log("Failure");
+// } else {
+//   console.log("Success");
+// }
+
+// Switch Statement
