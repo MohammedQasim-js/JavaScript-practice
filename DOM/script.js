c7522c8f3c25d081a5a9e9ced558aec78cbf1fdd
Mohammed Qasim <mohammed.qasim1702@gmail.com>
2025-08-04 17:56:32 +0530

    DOM

---
+++ DOM/script.js
@@ -0,0 +1,63 @@
+// var h1element = document.querySelector("h1");
+
+// h1element.style.color = "blue";
+
+// var isYellow = false;
+// var bodyElement = document.querySelector("body");
+
+// setInterval(function () {
+//   if (isYellow) {
+//     bodyElement.style.background = "white";
+//   } else {
+//     bodyElement.style.background = "yellow";
+//   }
+//   isYellow = !isYellow;
+// }, 1000);
+
+//* selection methods
+// var selectWithId = document.getElementById("JSparagraph");
+// console.log(selectWithId);
+
+// var selectWithClass = document.getElementsByClassName("background");
+// console.log(selectWithClass);
+
+// var selectWithTag = document.getElementsByTagName("h1");
+// console.log(selectWithTag);
+
+// var selectWithQueryId = document.querySelector("#JSparagraph");
+// console.log(selectWithQueryId);
+
+// var selectWithQueryClass = document.querySelector(".background");
+// console.log(selectWithQueryClass);
+
+// var selectWithQueryAllClass = document.querySelectorAll(".background");
+// console.log(selectWithQueryAllClass);
+
+//* Manipulation Methods
+// var divToManipulate = document.querySelector(".background");
+
+// divToManipulate.style.height = "50px";
+// divToManipulate.style.width = "400px";
+// divToManipulate.style.backgroundColor = "red";
+// divToManipulate.style.fontSize = "30px";
+// divToManipulate.style.border = "5px solid black";
+
+// divToManipulate.classList.add("addClass");
+// divToManipulate.classList.remove("addClass");
+// divToManipulate.classList.toggle("addClass");
+
+// var paragraph = document.querySelector("#JSparagraph");
+
+// paragraph.textContent = "We are learning javascript, here.";
+
+// paragraph.innerHTML = "We learning a <strong>great</strong> language, JavaScript.";
+
+//* Manipulate Attributes
+// var myLink = document.querySelector("a");
+
+// console.log(myLink.getAttribute("href"));
+
+// myLink.setAttribute("href", "https://github.com/MohammedQasim-js");
+// console.log(myLink.getAttribute("href"));
+
+// myLink.textContent = "My Github.";
