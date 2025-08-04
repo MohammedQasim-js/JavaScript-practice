6ad57a676934c0177da1b8618b4da888bef68d85
Mohammed Qasim <mohammed.qasim1702@gmail.com>
2025-08-04 19:03:50 +0530

    Event Handlers

--- DOM/script.js
+++ DOM/script.js
@@ -61,3 +61,65 @@
 // console.log(myLink.getAttribute("href"));
 
 // myLink.textContent = "My Github.";
+
+//* Event Handlers
+// var button = document.querySelector("#btn1");
+// var title = document.querySelector("#jsTitle");
+
+// button.addEventListener("click", function () {
+//   console.log("You clicked the Button!");
+//   title.style.backgroundColor = "red";
+// });
+
+// var listParent = document.querySelector("ul");
+
+// listParent.addEventListener("click", function () {
+//   console.log("You clicked the ul");
+//   listParent.style.backgroundColor = "blue";
+// });
+
+// var listItem = document.querySelectorAll("li");
+// console.log("ListItems:", listItem);
+
+// for (var i = 0; i < listItem.length; i++) {
+//   listItem[i].addEventListener("click", function (event) {
+//     event.target.style.color = "white";
+//   });
+// }
+
+// var jsParagraph = document.querySelector("#JSparagraph");
+
+// function toggleBackground() {
+//   jsParagraph.classList.toggle("toggleBackground");
+// }
+
+// jsParagraph.addEventListener("click", toggleBackground);
+
+// var firstTodoItem = document.querySelector(".todoItem");
+// firstTodoItem.addEventListener("mouseover", function () {
+//   firstTodoItem.style.backgroundColor = "blue";
+//   firstTodoItem.style.color = "white";
+// });
+
+// firstTodoItem.addEventListener("mouseout", function () {
+//   firstTodoItem.style.backgroundColor = "white";
+//   firstTodoItem.style.color = "black";
+// });
+
+var todoListItems = document.querySelectorAll(".todoItem");
+
+for (var i = 0; i < todoListItems.length; i++) {
+  todoListItems[i].addEventListener("mouseover", function () {
+    this.style.backgroundColor = "blue";
+    this.style.color = "white";
+  });
+
+  todoListItems[i].addEventListener("mouseout", function () {
+    this.style.backgroundColor = "white";
+    this.style.color = "black";
+  });
+
+  todoListItems[i].addEventListener("click", function () {
+    this.classList.add("okay");
+  });
+}
